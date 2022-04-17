0a0ae5c5322df8617c78cde382f6960f751a9807
Entangle89 <p.tinctoria89@gmail.com>
2022-04-17 22:58:10 +0900

    タイル状の床を生成するシステムを作成

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,28 @@ use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
 //use bevy_kira_audio::{Audio, AudioPlugin};
 
+#[derive(Component)]
+struct Position(Vec3);
+
+#[derive(Component)]
+struct Tile;
+
+#[derive(Component)]
+struct Floor;
+
+#[derive(Component)]
+struct Size {
+    width: u32,
+    depth: u32,
+}
+
+#[derive(Bundle)]
+struct FloorBundle {
+    floor: Floor,
+    size: Size,
+    position: Position,
+}
+
 fn main() {
     App::new()
         .insert_resource(Msaa { samples: 4 })
@@ -10,6 +32,8 @@ fn main() {
         .add_plugin(WorldInspectorPlugin::new())
         //.add_startup_system(start_background_audio)
         .add_startup_system(setup)
+        .add_startup_system_to_stage(StartupStage::Startup, spawn_floor)
+        .add_startup_system_to_stage(StartupStage::PostStartup, spawn_tiles)
         .run();
 }
 
@@ -18,12 +42,8 @@ fn main() {
 //audio.play_looped(asset_server.load("Lady_Maria.mp3"));
 //}
 
-/// set up a simple 3D scene
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+// set up a simple 3D scene
+fn setup(mut commands: Commands) {
     // set up the camera
     let mut camera = OrthographicCameraBundle::new_3d();
     camera.orthographic_projection.scale = 10.0;
@@ -32,31 +52,6 @@ fn setup(
     // camera
     commands.spawn_bundle(camera);
 
-    // cubes
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(1.5, 0.0, 1.5),
-        ..Default::default()
-    });
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(1.5, 0.0, -1.5),
-        ..Default::default()
-    });
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(-1.5, 0.0, 1.5),
-        ..Default::default()
-    });
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(-1.5, 0.0, -1.5),
-        ..Default::default()
-    });
     // light
     commands.spawn_bundle(DirectionalLightBundle {
         transform: Transform::from_xyz(-3.0, 8.0, -3.0).looking_at(Vec3::ZERO, Vec3::Y),
@@ -68,4 +63,40 @@ fn setup(
         },
         ..Default::default()
     });
+}
+
+fn spawn_floor(mut commands: Commands) {
+    commands.spawn_bundle(FloorBundle {
+        floor: Floor,
+        size: Size { width: 5, depth: 4 },
+        position: Position(Vec3::new(0.0, 0.0, 0.0)),
+    });
+}
+
+fn spawn_tiles(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    query: Query<(&Size, &Position), With<Floor>>,
+) {
+    let mesh = Mesh::from(shape::Cube { size: 1.0 });
+    let material = StandardMaterial::from(Color::rgb(0.8, 0.7, 0.6));
+
+    for (size, position) in query.iter() {
+        let y = position.0.y;
+        for i in 0..size.width {
+            for j in 0..size.depth {
+                let x = (i as f32) - 0.5 * (size.width as f32) + 0.5;
+                let z = (j as f32) - 0.5 * (size.depth as f32) + 0.5;
+                commands
+                    .spawn_bundle(PbrBundle {
+                        mesh: meshes.add(mesh.clone()),
+                        material: materials.add(material.clone()),
+                        transform: Transform::from_translation(Vec3::new(x, y, z)),
+                        ..Default::default()
+                    })
+                    .insert(Tile);
+            }
+        }
+    }
 }
