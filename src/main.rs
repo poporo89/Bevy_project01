0413e0836d22969dac9bec317fbe26a7c739cada
Entangle89 <p.tinctoria89@gmail.com>
2022-04-17 22:56:42 +0900

    カメラの設定を変更

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,8 @@ fn setup(
 ) {
     // set up the camera
     let mut camera = OrthographicCameraBundle::new_3d();
-    camera.orthographic_projection.scale = 3.0;
-    camera.transform = Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y);
+    camera.orthographic_projection.scale = 10.0;
+    camera.transform = Transform::from_xyz(-10.0, 10.0, -10.0).looking_at(Vec3::ZERO, Vec3::Y);
 
     // camera
     commands.spawn_bundle(camera);
