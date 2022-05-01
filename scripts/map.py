622b4ecd228fe72de759166cc7de1006b618430f
Entangle89 <p.tinctoria89@gmail.com>
2022-05-02 05:33:38 +0900

    テストマップに2階を追加

--- scripts/map.py
+++ scripts/map.py
@@ -4,10 +4,17 @@ def test_map():
     # 1st floor
     height = 0
     data = [
-        [ 0, 0, 0, 0,],
+        [ 0, 1, 2, 3,],
         [ 0,-1,-1, 0,],
         [ 0, 0, 0, 0,],
     ]
     result.append({'height':height, 'data':data})
+    # 2nd floor
+    height = 3
+    data = [
+        [-1,-1,-1,-1,],
+        [-1,-1,-1, 0,],
+        [-1,-1,-1, 0,],
+    ]
     result.append({'height':height, 'data':data})
     return result
